99df2edd5cd9776b8c1ead5b71b7fe297f67102d
Dauntless7 <nicolai.8293@gmail.com>
2021-01-22 18:51:12 +0000

    package

--- src/Client/events/ready.ts
+++ src/Client/events/ready.ts
@@ -3,5 +3,10 @@ export const onReady: Event = {
   packetName: 'ready',
   handler() {
     console.log('ready');
+    this.editStatus(null, {
+      name: 'pp help',
+      type: 1,
+      url: 'https://www.twitch.tv/twitch'
+    });
   }
 };
