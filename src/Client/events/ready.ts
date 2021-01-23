1412b2c1d777c37edb793fbc645ea6281f63fdd4
tintin9999 <parthk134@gmail.com>
2021-01-23 14:31:28 +0530

    imagine committing a conflict without resolving it @Dauntless7

--- src/Client/events/ready.ts
+++ src/Client/events/ready.ts
@@ -4,11 +4,7 @@ export const onReady: Event = {
   handler() {
     console.log('ready');
     this.editStatus(null, {
-<<<<<<< HEAD
       name: 'pp help',
-=======
-      name: 'pp create',
->>>>>>> 3c1bb4e92d1f3940820b078b6d1dae3a4d451ec0
       type: 1,
       url: 'https://www.twitch.tv/twitch'
     });
